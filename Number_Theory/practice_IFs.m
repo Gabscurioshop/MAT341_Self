486dce2a6173faee1e05026e5e629a1d35fde9ff
Gabrielle Curcio <33849234+Gabscurioshop@users.noreply.github.com>
2019-09-27 20:24:44 -0400

    updated exercises from 9/24 and 9/27 classes

---
+++ Number_Theory/practice_IFs.m
@@ -0,0 +1,62 @@
+function practice_IFs()
+
+x = 8;
+
+if x < 5
+    A = 10;
+else
+    A = 57.5;
+    
+end
+
+y = 37;
+
+if y < 5
+    B = 2*y-1;
+elseif y < 10
+    B = 3*y^2+2;
+end
+
+x = 3;
+if x<2
+    C = 1;
+end    
+if x < 5
+    C =2;
+end
+if x<10
+    C =3;
+end
+if x<100
+    C =4;
+end
+if x < 1000
+    C = 5;
+end
+C
+
+%less than or equal to
+if x <= y
+    A =1;
+end
+%greater than or equal to
+if x >= y
+    A = 2;
+end
+%equal to (two equal signs)
+if x == y
+    A =3;
+end
+%Not equal to
+if x ~= y
+    A =4;
+end
+%AND (if both statements are true
+if((x<=y) && (x~=y))
+    A = 5;
+end
+
+%OR (if either statement or both is true do something
+if((x>y) || (x<y))
+    A = 6;
+end
