77c8bb333e153df7bcf9fa0097e416d5e367dffe
Gabscurioshop <curciog1@tcnj.edu>
2019-10-29 13:32:01 -0400

    updated comments for HW4 scripts

--- HWS/HW4/successive_Parabolic_Interpolation.m
+++ HWS/HW4/successive_Parabolic_Interpolation.m
@@ -10,15 +10,12 @@ N = 0;
 %3 points within our interval
 x1 = 0;
 x2 = 0.6;
-x3 = 2;
+x3 = 1.2;
 %solve for the y coordinates
 f1 = f(x1);
 f2 = f(x2);
 f3 = f(x3);
 %coefficient matrix based on the linear system of eqns
-%f1 = a(x1^2) +bx1+c
-%f2 = a(x2^2) +bx2+c
-%f3 = a(x3^2) +bx3+c
 A = [ x1^2 x1  1;
      x2^2 x2 1;
       x3^2 x3  1];
@@ -37,9 +34,11 @@ x2 = x1;
 x1 = xMin;
 
 while abs(x3-x1) > tol
+    %solve for the y coordinates with new set of x1,x2,x3
     f1 = f(x1);
     f2 = f(x2);
     f3 = f(x3);
+    %set up matrices
     A = [ x1^2 x1  1;
      x2^2 x2 1;
       x3^2 x3  1];
@@ -48,7 +47,9 @@ while abs(x3-x1) > tol
     a = X(1);
     b = X(2);
     c = X(3);
+    %find the vertex
     xMin = (b*-1)/(2*a);
+    %reassign values
     x3 = x2;
     x2 = x1; 
     x1 = xMin;
@@ -58,8 +59,9 @@ N
 %function
 function val = f(x)
 val = 0.5-x*exp(-x^2);
-%It takes 6 iterations to achieve 1e-8 accuracy
-%After changing x3 from 0.9 to 2, the number of trials increased by 4. 
+%a)It takes 6 iterations to achieve 1e-8 accuracy
+%b)After changing x3 from 0.9 to 2, it now takes 10 iterations 
+%to achieve 1e-8 accuracy. 
 %MATLAB issues this following warning: Matrix is close to 
 %singular or badly scaled. Results may be inaccurate.
 %This means that the coefficients in the linear system are getting closer
--- HWS/HW4/vary_Error_Tolerances_To_Compare.m
+++ HWS/HW4/vary_Error_Tolerances_To_Compare.m
@@ -1,12 +1,14 @@
 function vary_Error_Tolerances_To_Compare()
-
+%this function plots 2 graphs that display the relationship
+%the error tolerances and the number of iterations for 
+%golden _search and successive parabolic interploation
 %error tolerance vector
-errTolVec= [1e-1 1e-2  1e-3  1e-4  1e-5  1e-6  1e-7  1e-8  1e-9  1e-10  1e-11  1e-12];
+errTolVec= [1e-1 1e-2  1e-3  1e-4  1e-5  1e-6  1e-7  1e-8  1e-9  1e-10  1e-11  1e-12 1e-13 1e-14 1e-15];
 
 len = length(errTolVec);%length of errTolVec
 
 for i=1:len
-    %calculates probability for each radius in vector
+    %calculates probability for each error tolerance in vector
     x(i) = errTolVec(i);%gets the element at index i in errTolVec
     gsVec(i) = golden_Search(x(i)); %number of trials from golden search
     spiVec(i) = successive_Parabolic_Interpolation(x(i));
@@ -14,17 +16,30 @@ for i=1:len
 end
 
 gsfig = figure
+%plots two figures on semilog graph
 semilogx(x,gsVec,'bo-')%plots points in blue
 hold on; %this plots all the points calculated by the function
-semilogx(x,spiVec,'ro-')%plots points in blue
+semilogx(x,spiVec,'ro-')%plots points in red
 legend('Golden Search','Succ. Para. Interp.')
 set(0, 'DefaultLineLineWidth', 5);% sets the line width of graph
 spffig = figure
+%plots two figures on loglog graph
 loglog(x,gsVec,'bo-');%plots points in blue
 hold on;
-loglog(x,spiVec,'ro-')%plots points in blue
+loglog(x,spiVec,'ro-')%plots points in red
 hold on;
 %labels the axes
 xlabel('error tolerance, tol')
 ylabel('# of iterations, N')
 legend('Golden Search','Succ. Para. Interp.')
+set(0, 'DefaultLineLineWidth', 5);% sets the line width of graph
+%a)For less accurate tolerances, the successive
+%parabolic interploation algorithm converges faster to the minimum.
+%b)When increasing the accuracy threshold, the convergence
+%rate of the golden search algorithm remains unchanged while the
+%convergence rate for successive parabolic interpolation changes.
+%Successive parabolic interpolation algorithm doesn't always converge
+%quicker. This is the case because that algorithm uses matrix
+%multiplication to solve for the coefficients of a parabola.
+%As your calculating a larger number of matrix calculations, 
+%the coefficients get closer to zero. 
